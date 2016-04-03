29219b03e3fb822a19e190cb37053864a2f2630e
Contextualist <harzjc@gmail.com>
2016-04-03 18:43:40 +0800

    Created by Coding Ground Client

---
+++ Quip-Assign/test.py
@@ -0,0 +1,7 @@
+import quip
+
+client = quip.QuipClient(access_token="Wk9EQU1BcDZFS04=|1483091850|CF037JVoITJPnAET8aHWnZwEZACvrIm7jtkRIQCaX3g=")
+theID = "Z0R5AhbLjUxu" # test doc 0309-c
+
+client.edit_document(thread_id=theID, content=r"Hi", format="markdown", 
+                             operation=client.APPEND)
